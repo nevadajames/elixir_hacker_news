3074cf15184cb1598ef5b4f4ef559a1f8835548a
Nevada James Start <nevadajames@gmail.com>
2019-07-10 19:20:55 +0200

    increment version and generate docs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HackerNewsClient.MixProject do
   def project do
     [
       app: :hacker_news_client,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
